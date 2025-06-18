260d0dd0459bbe22b1f2fba270ded46ed0df1a58
rubberPlant256 <ihrustal@yandex.ru>
2025-06-18 21:32:37 +0300

    что-то

--- src/main/java/org/ficus/data/repository/ScheduleRepository.java
+++ src/main/java/org/ficus/data/repository/ScheduleRepository.java
@@ -3,5 +3,9 @@ package org.ficus.data.repository;
 import org.ficus.data.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    List<Schedule> findAll();
 }
--- src/main/java/org/ficus/service/ScheduleService.java
+++ src/main/java/org/ficus/service/ScheduleService.java
@@ -1,5 +1,19 @@
 package org.ficus.service;
 
+import lombok.RequiredArgsConstructor;
+import org.ficus.data.entity.Schedule;
+import org.ficus.data.repository.ScheduleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ScheduleService {
 
+    private final ScheduleRepository scheduleRepository;
+
+    public List<Schedule> getAllSchedules() {
+        return scheduleRepository.findAll();
+    }
 }
